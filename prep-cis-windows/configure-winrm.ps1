9cc47f45940902b46c9cb1d93247d69406230f37
Cesar Younes <cesar.younes@outlook.com>
2020-06-07 13:27:01 +0400

    Create cis image

--- prep-cis-windows/configure-winrm.ps1
+++ prep-cis-windows/configure-winrm.ps1
@@ -6,28 +6,23 @@ Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
 # Don't set this before Set-ExecutionPolicy as it throws an error
 $ErrorActionPreference = "stop"
 
+cmd.exe /c winrm quickconfig -q
+
 # Remove HTTP listener
 Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
-
-# Create a self-signed certificate to let ssl work
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName "packer"
-New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Cert.Thumbprint -Force
 
 # WinRM
 Write-Output "Setting up WinRM"
 Write-Host "(host) setting up WinRM"
 
-cmd.exe /c winrm quickconfig -q
-cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
-cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
-cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
-cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
-cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTPS" "@{Port=`"5986`";Hostname=`"packer`";CertificateThumbprint=`"$($Cert.Thumbprint)`"}"
-cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-cmd.exe /c netsh firewall add portopening TCP 5986 "Port 5986"
+Set-ItemProperty HKLM:\Software\Policies\Microsoft\Windows\WinRM\Service -name AllowBasic -Value 1
+Set-ItemProperty HKLM:\Software\Policies\Microsoft\Windows\WinRM\Client -name AllowBasic -Value 1
+
+Set-ItemProperty HKLM:\Software\Policies\Microsoft\Windows\WinRM\Service -name AllowUnencryptedTraffic -Value 1
+Set-ItemProperty HKLM:\Software\Policies\Microsoft\Windows\WinRM\Client -name AllowUnencryptedTraffic -Value 1
+
+New-NetFirewallRule -Direction Inbound -Action Allow -DisplayName "Windows Remote Management [HTTPS-In]" -Description "Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]" -Program "System" -Profile Domain,Private -Protocol TCP -LocalPort "5986" -RemotePort Any
+
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
--- prep-cis-windows/create-cis-image.ps1
+++ prep-cis-windows/create-cis-image.ps1
@@ -44,47 +44,24 @@ Set-AzVMCustomScriptExtension `
     -FileUri "https://storageonmsdn.blob.core.windows.net/packer/configure-winrm.ps1" `
     -Run 'configure-winrm.ps1'
 
-#Create a snapshot of Windows VM disk
-
 #Initialize Variables to be used
-$resourceGroupName = 'rg-adp-simulation-templates' 
-$location = 'westeurope' 
-$vmName = 'simwinbase'
-$snapshotName = 'cisWindowsServer2019WinRMBasicAuth'
-$storageAccountName = 'storageadpsimtemplates'
-$managedDiskId = '/subscriptions/5923282c-c394-42a1-9a37-9e898a8e16a7/resourceGroups/RG-ADP-SIMULATION-TEMPLATES/providers/Microsoft.Compute/disks/simwinbase_OsDisk_1_744db9c652d143c3b5f2c156f7e2765f'
+$vmName = "ciswinprep"
+$ResourceGroupName = "ciswinprep-rg"
+# $storageAccountName = 'storageonmsdn'
+$vm = Get-AzVM -Name $VMName `
+   -ResourceGroupName $ResourceGroupName
 
-$vm = az vm get-instance-view -g $resourceGroupName -n $vmName
-$storageAccount = az storage account show -g $resourceGroupName -n $storageAccountName
+$disk = Get-AzDisk -ResourceGroupName $resourceGroupName `
+  -DiskName $vm.StorageProfile.OsDisk.Name
 
-$snapshot =  New-AzSnapshotConfig -SourceUri $managedDiskId -Location $location -CreateOption copy -StorageAccountId $storageAccount.Id
+$snapshot =  New-AzSnapshotConfig -SourceUri $disk.Id -Location $LocationName -CreateOption copy -OsType Windows
 
-New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $resourceGroupName
+New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $ResourceGroupName
 
-
-#Provide the subscription Id of the subscription where snapshot is created
-$subscriptionId = "5923282c-c394-42a1-9a37-9e898a8e16a7"
-
-#Provide Shared Access Signature (SAS) expiry duration in seconds e.g. 3600.
-#Know more about SAS here: https://docs.microsoft.com/en-us/Az.Storage/storage-dotnet-shared-access-signature-part-1
-$sasExpiryDuration = "3600"
-#Name of the storage container where the downloaded snapshot will be stored
-$storageContainerName = "winsimsnapshots"
-
-#Provide the key of the storage account where you want to copy snapshot. 
-$storageAccountKey = 'X09iwqpsHHg7tIIEH/p2ldiLahBiz5hoM02obJOxqdCSaq9HiaHGsHPZyWxl5N5g55IQ4yy+vD8Tars+YboigA=='
-
-#Provide the name of the VHD file to which snapshot will be copied.
-$destinationVHDFileName = "cisWindowsServer2019WinRMBasicAuth.vhd"
-
-
-# Set the context to the subscription Id where Snapshot is created
-Select-AzSubscription -SubscriptionId $SubscriptionId
-
-#Generate the SAS for the snapshot 
-$sas = Grant-AzSnapshotAccess -ResourceGroupName $ResourceGroupName -SnapshotName $SnapshotName  -DurationInSecond $sasExpiryDuration -Access Read
-#Create the context for the storage account which will be used to copy snapshot to the storage account 
-$destinationContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
-
-#Copy the snapshot to the storage account 
-Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $storageContainerName -DestContext $destinationContext -DestBlob $destinationVHDFileName
+$ImageName = "cis-pwsh-vhd-20200604"
+$LocationName = "westeurope"
+$VMName = "ciswinprep"
+$ResourceGroupName = "ciswinprep-rg"
+$vm = Get-AzVM -Name $VMName -ResourceGroupName $ResourceGroupName
+$image = New-AzImageConfig -Location $LocationName -SourceVirtualMachineId $vm.Id
+New-AzImage -Image $image -ImageName $imageName -ResourceGroupName $ResourceGroupName
